99589f612597e2ee34c3febd90496385a9536f2d
schikkam <shiva.chikkam@gmail.com>
2018-12-12 15:42:07 +0530

    sshd restart.sh

---
+++ sshd_restart.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Desc: Script To Start SSH Service If Not Running
+op=`sudo service sshd status`
+OUT="/var/log/sshd_service_log"
+echo $op
+if [[ $op == *"running"* ]]; 
+then  
+ echo "`date +%D+%T` : SSH Service Is Healthy" >> $OUT
+else
+ echo "`date +%D+%T` : SSH Service Is Not Healthy, So Restarting.." >> $OUT
+ service sshd restart 
+ if [[ $? != 0 ]]; then
+   echo "`date +%D+%T` : Problem In Restarting SSHD Services, Please Check" >> $OUT
+ fi
+fi
+
