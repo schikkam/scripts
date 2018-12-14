0329b85dd643544a858546b3821b0bce54917abb
schikkam <shiva.chikkam@gmail.com>
2018-12-14 10:08:35 +0530

    Missing sudo for starting service

--- sshd_restart.sh
+++ sshd_restart.sh
@@ -7,7 +7,7 @@ then
  echo "`date +%D+%T` : SSH Service Is Healthy" >> $OUT
 else
  echo "`date +%D+%T` : SSH Service Is Not Healthy, So Restarting.." >> $OUT
- service sshd restart 
+ sudo service sshd restart 
  if [[ $? != 0 ]]; then
    echo "`date +%D+%T` : Problem In Restarting SSHD Services, Please Check" >> $OUT
  fi
